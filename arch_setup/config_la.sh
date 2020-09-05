e8fe7be64618cc9e5d32a68c97a739bd33ffdd83
ggzor <30713864+ggzor@users.noreply.github.com>
2020-09-05 15:56:24 -0500

    Adding my preferred configuration

---
+++ arch_setup/config_la.sh
@@ -0,0 +1,8 @@
+# My preferred settings for installation
+# This file can be sourced to set the variables to these values
+
+export LOCALE=en_US.UTF-8
+export ENCODING=UTF-8
+export TIMEZONE=America/Mexico_City
+export KEYMAP=la-latin1
+
