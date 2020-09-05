22931a7d7b67c3f90021adbd7dbdec2494af780d
ggzor <30713864+ggzor@users.noreply.github.com>
2020-09-05 15:35:16 -0500

    Fixing wireless mandatory setup

--- arch_setup/install_packages.sh
+++ arch_setup/install_packages.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 # Load packages from list
 PACKAGES=$(cat './arch_setup/packages.txt' | sed '/^$/d' | grep -v '^#' | tr '\n' ' ')
 
--- arch_setup/packages.sh
+++ arch_setup/packages.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 # Systemd commands
 systemctl enable dhcpcd.service iwd.service \
                  sshd.service wpa_supplicant.service
 
 # Configure supplicant
 wpa_supplicant -B -i wlan0 \
-               -c <(echo "ctrl_interface=/run/wpa_supplicant\nupdate_config=1")
+               -c <(echo "ctrl_interface=/run/wpa_supplicant\nupdate_config=1") \
+               || echo "No wireless interface found. Omitting setup."
 
