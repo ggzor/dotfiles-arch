676d7f9d991113d85688b42054990db147ef217e
ggzor <30713864+ggzor@users.noreply.github.com>
2020-09-05 19:54:08 -0500

    Removing wpa_supplicant initialization as it disconnects wifi

--- arch_setup/packages.sh
+++ arch_setup/packages.sh
@@ -6,8 +6,3 @@ set -euo pipefail
 systemctl enable dhcpcd.service iwd.service \
                  sshd.service wpa_supplicant.service
 
-# Configure supplicant
-wpa_supplicant -B -i wlan0 \
-               -c <(echo "ctrl_interface=/run/wpa_supplicant\nupdate_config=1") \
-               || echo "No wireless interface found. Omitting setup."
-
