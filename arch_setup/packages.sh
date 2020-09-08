266d4dc46f68add8e438d161fc815c859f9c7b1b
ggzor <30713864+ggzor@users.noreply.github.com>
2020-09-08 14:39:34 -0500

    Adding extra development packages and utilities

--- arch_setup/packages.sh
+++ arch_setup/packages.sh
@@ -5,6 +5,20 @@ set -euo pipefail
 # Systemd commands
 systemctl enable sshd.service
 
+## Docker
+# Enable docker service
+systemctl enable docker.service
+# Start docker
+systemctl start docker.service
+# Add user to docker group
+usermod -a -G docker "${USER_NAME:-$SUDO_USER}"
+
 # Set xorg keyboard layout
 [ -v "XORG_KEYMAP" ] && localectl set-x11-keymap "$XORG_KEYMAP"
+
+# Set user shell
+chsh -s "`which zsh`" "${USER_NAME:-$SUDO_USER}"
+
+# Install stable rust toolchain
+sudo -u "${USER_NAME:-$SUDO_USER}" rustup default stable
 
