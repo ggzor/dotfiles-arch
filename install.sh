47749ca3135626b3b50b493af40a2a01094ba033
ggzor <30713864+ggzor@users.noreply.github.com>
2020-09-26 13:46:46 -0500

    Adding scripts to install configuration

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euo pipefail
+
+source './utils.sh'
+
+# Link configuration files and folders using utils.sh
+link_same        "$(pwd)/awesome"      "$HOME/.config/awesome"
+link_same        "$(pwd)/kitty"        "$HOME/.config/kitty"
+link_same_files  "$(pwd)/nvim"         "$HOME/.config/nvim"
+link_same_single "$(pwd)" 'picom.conf' "$HOME/.config/picom"
+link_same_single "$(pwd)" '.xinitrc'   "$HOME"
+
+echo -e "\033[0;32mLinked all configuration files\033[0m"
+
---
+++ utils.sh
@@ -0,0 +1,39 @@
+# Utilities for other scripts
+
+set -euo pipefail
+
+# Usage:
+# link_same TARGET LINK_NAME
+#
+# Create a link to TARGET only if LINK_NAME does not exist
+# if LINK_NAME exists and links to TARGET, then do nothing
+# otherwise, fail
+link_same() {
+  (ln -sT "$1" "$2" &> /dev/null && echo "Linked $2 -> $1") \
+   || ([ "$(readlink "$2")" = "$1" ] && echo "Already linked $2") \
+   || (echo -e "\033[0;31mUnable to link $2 -> $1\033[0m" && false)
+}
+
+# Usage:
+# link_same_files SOURCE_DIR TARGET_DIR
+#
+# Similar to link_same, but links each file inside of TARGET
+link_same_files() {
+  mkdir -p "$2"
+
+  for f in $(ls "$1"); do
+    link_same "$1/$f" "$2/$f"
+  done
+}
+
+# Usage
+# link_same_single SOURCE_DIR FILENAME TARGET_DIR
+#
+# Links the file with the semantics of link_same and creates
+# the directory if not exists
+link_same_single() {
+  mkdir -p "$3"
+
+  link_same "$1/$2" "$3/$2"
+}
+
