85e7afb309954d846fe805917eff13151d235a85
ggzor <30713864+ggzor@users.noreply.github.com>
2020-09-29 22:32:48 -0500

    Organizing packages and updating install script

--- arch_setup/pacman/install.sh
+++ arch_setup/pacman/install.sh
@@ -4,7 +4,8 @@ set -euo pipefail
 
 # Load packages from list
 PACKAGES=$(cat './arch_setup/pacman/packages.txt' \
-          | sed '/^$/d' | grep -v '^#' | tr '\n' ' ')
+          | sed '/^$/d' | grep -v '^#' | awk '{print $1}' \
+          | tr '\n' ' ')
 
 # Install packages
 pacman -Syu --noconfirm
