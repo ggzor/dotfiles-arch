a5e0183990ce80f43de2c9aad2e0666c19c39041
ggzor <30713864+ggzor@users.noreply.github.com>
2020-10-19 16:08:11 -0500

    fnm setup command doesn't requires root permissions

--- arch_setup/yay/config.sh
+++ arch_setup/yay/config.sh
@@ -5,5 +5,5 @@ set -euo pipefail
 # Extra commands after installing AUR packages
 
 # Install nodejs version and make the default
-sudo -u "$USER_NAME" bash -c 'fnm install 14.10.1 && fnm default 14.10.1'
+fnm install 14.10.1 && fnm default 14.10.1
 
