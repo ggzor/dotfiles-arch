3d86782464406219d62a8b14092afe58d0db8885
ggzor <30713864+ggzor@users.noreply.github.com>
2020-10-19 16:52:57 -0500

    Adding .config folder creation to support fresh installs

--- arch_setup/yay/config.sh
+++ arch_setup/yay/config.sh
@@ -4,6 +4,9 @@ set -euo pipefail
 
 # Extra commands after installing AUR packages
 
+# Fresh install doesn't have this folder
+mkdir -p "$HOME/.config"
+
 # Install nodejs version and make the default
 fnm install 14.10.1 && fnm default 14.10.1
 
