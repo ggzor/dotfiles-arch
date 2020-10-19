c84aa7d89f63ef5a8255bc82c4c22d261904dc88
ggzor <30713864+ggzor@users.noreply.github.com>
2020-10-19 17:02:57 -0500

    Updating nodejs version and installation with fnm

--- arch_setup/yay/config.sh
+++ arch_setup/yay/config.sh
@@ -5,5 +5,8 @@ set -euo pipefail
 # Extra commands after installing AUR packages
 
 # Install nodejs version and make the default
-fnm install 14.10.1 && fnm default 14.10.1
+NODE_VERSION=14.14.0
+fnm install $NODE_VERSION \
+  && fnm use $NODE_VERSION \
+  && fnm default $NODE_VERSION
 
