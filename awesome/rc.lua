313072cb2593f33fdcd4c3022958f3081464b6f8
ggzor <30713864+ggzor@users.noreply.github.com>
2020-11-04 10:12:39 -0600

    Adding print screen shortcut with flameshot

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -193,6 +193,8 @@ globalkeys = gears.table.join(
     -- Standard program
     awful.key({ modkey,           }, "Return", function () awful.spawn(terminal) end,
               {description = "open a terminal", group = "launcher"}),
+    awful.key({ }, "Print", function () awful.spawn("flameshot gui") end,
+              {description = "take a screenshot with gui", group = "launcher"}),
     awful.key({ modkey, "Control" }, "r", awesome.restart,
               {description = "reload awesome", group = "awesome"}),
     awful.key({ modkey, "Shift"   }, "q", awesome.quit,
