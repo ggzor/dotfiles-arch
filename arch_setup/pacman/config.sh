65eb06fcaebfb0e9c998f7ba0608dce53477d4ce
ggzor <30713864+ggzor@users.noreply.github.com>
2021-03-05 11:54:50 -0600

    Replacing pulseaudio with pipewire

--- arch_setup/pacman/config.sh
+++ arch_setup/pacman/config.sh
@@ -6,10 +6,6 @@ export USER_NAME=${USER_NAME:-$SUDO_USER}
 # Systemd commands
 systemctl enable sshd.service
 
-# Pulseaudio
-systemctl --user enable pulseaudio
-systemctl --user start pulseaudio
-
 # firewalld
 systemctl enable firewalld
 systemctl start firewalld
