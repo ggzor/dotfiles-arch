848701d6e4d312fd234baa4a64cdb31a64a321fc
ggzor <30713864+ggzor@users.noreply.github.com>
2021-04-20 12:17:18 -0500

    Removing more packages

--- arch_setup/pacman/config.sh
+++ arch_setup/pacman/config.sh
@@ -10,14 +10,3 @@ systemctl enable sshd.service
 systemctl enable firewalld
 systemctl start firewalld
 
-## Docker
-# Enable docker service
-systemctl enable docker.service
-# Start docker
-systemctl start docker.service
-# Add user to docker group
-usermod -a -G docker "$USER_NAME"
-
-# Install stable rust toolchain
-sudo -u "$USER_NAME" rustup default stable
-
