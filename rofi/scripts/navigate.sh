696c67b297abd7c30884325d8da1e5387d67809b
ggzor <30713864+ggzor@users.noreply.github.com>
2021-04-20 21:00:14 -0500

    Adding tag aware browser opening

--- rofi/scripts/navigate.sh
+++ rofi/scripts/navigate.sh
@@ -19,11 +19,28 @@ SITES=(
   [" figma"]="https://figma.com"
   [" outlook mail"]="https://outlook.live.com"
   [" office outlook mail"]="https://outlook.office.com/"
+  [" nerd fonts cheat sheet"]="https://www.nerdfonts.com/cheat-sheet"
 )
+
+CHECK_BROWSER=$(cat <<EOF
+for _, x in ipairs(mouse.screen.selected_tag:clients()) do
+  if x.role == 'browser' then
+    return 'browser'
+  end
+end
+return 'nobrowser'
+EOF
+)
+
+# Check if any browser is visible
+EXTRA_PARAMS=""
+if [[ $(awesome-client <<< "$CHECK_BROWSER") == *"nobrowser"* ]]; then
+  EXTRA_PARAMS+='--new-window'
+fi
 
 if [ "$@" ]
 then
-    xdg-open "${SITES["$*"]}" &> /dev/null &
+    google-chrome-stable $EXTRA_PARAMS "${SITES["$@"]}" &> /dev/null &
 else
     echo -en "\x00prompt\x1f<span fgcolor='#6c7a89'>go to</span>\n"
     echo -en "\x00markup-rows\x1ftrue\n"
