252eef6f3d7fb430aee4a6d70ef68de5bae0aa3d
ggzor <30713864+ggzor@users.noreply.github.com>
2021-06-05 18:42:03 -0500

    Adding acpid package

--- arch_setup/pacman/config.sh
+++ arch_setup/pacman/config.sh
@@ -21,3 +21,7 @@ usermod -a -G docker "$USER_NAME"
 # Install stable rust toolchain
 sudo -u "$USER_NAME" rustup default stable
 
+# Enable acpid
+systemctl enable acpid.service
+systemctl start acpid.service
+
