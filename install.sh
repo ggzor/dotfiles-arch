97697576c790f57989e317671088b5078499edeb
ggzor <30713864+ggzor@users.noreply.github.com>
2021-06-13 20:23:40 -0500

    Organizing configuration files linking script

--- install.sh
+++ install.sh
@@ -14,21 +14,22 @@ link_same        "$(pwd)/wallpapers"   "$HOME/Pictures/wallpapers"
 # Link just files instead of entire folders to avoid
 # polluting this dotfiles directory with generated files
 link_same_files  "$(pwd)/nvim"         "$HOME/.config/nvim"
-link_same_files  "$(pwd)/xdg"          "$HOME/.config"
 link_same_files  "$(pwd)/vscode"       "$HOME/.config/Code - Insiders/User"
+link_same_files  "$(pwd)/xdg"          "$HOME/.config"
 
 # Link just one file
-link_same_single "$(pwd)" '.inputrc'   "$HOME"
 link_same_single "$(pwd)" '.gitconfig' "$HOME"
+link_same_single "$(pwd)" '.inputrc'   "$HOME"
 link_same_single "$(pwd)" '.p10k.zsh'  "$HOME"
-link_same_single "$(pwd)" 'picom.conf' "$HOME/.config"
-link_same_single "$(pwd)" '.tmux.conf' "$HOME"
 link_same_single "$(pwd)" '.theme.zsh' "$HOME"
+link_same_single "$(pwd)" '.tmux.conf' "$HOME"
 link_same_single "$(pwd)" '.xinitrc'   "$HOME"
-link_same_single "$(pwd)" 'zathurarc'  "$HOME/.config/zathura"
 link_same_single "$(pwd)" '.zinit.zsh' "$HOME"
 link_same_single "$(pwd)" '.zshrc'     "$HOME"
+link_same_single "$(pwd)" 'picom.conf' "$HOME/.config"
+link_same_single "$(pwd)" 'zathurarc'  "$HOME/.config/zathura"
 
+# Use same configuration file for neovim and vim
 link_same "$(pwd)/nvim/init.vim" "$HOME/.vimrc"
 
 echo -e "\033[0;32mLinked all configuration files\033[0m"
