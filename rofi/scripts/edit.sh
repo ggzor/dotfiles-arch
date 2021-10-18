aa68fa0ad2ca61819c293403751aef83f0915354
ggzor <30713864+ggzor@users.noreply.github.com>
2021-10-17 20:23:42 -0500

    Adding comment about prompt span tag

--- rofi/scripts/edit.sh
+++ rofi/scripts/edit.sh
@@ -10,6 +10,9 @@ if [ "$@" ]
 then
     kitty --title '<floating>' nvim "${FILES["$*"]}" &> /dev/null &
 else
+    # The label color is intentional, because it allows to fade the
+    # type of command, the tradeoff is that a non-combi mode has to be
+    # specified as a combi mode to allow to change the prompt.
     echo -en "\x00prompt\x1f<span fgcolor='#6c7a89'>edit</span>\n"
     echo -en "\x00markup-rows\x1ftrue\n"
 
