c70d9af2ea5f538189cfbe4205d1d7d502aeced3
ggzor <30713864+ggzor@users.noreply.github.com>
2021-11-24 23:42:13 -0600

    Updating node version

--- arch_setup/yay/config.sh
+++ arch_setup/yay/config.sh
@@ -9,9 +9,9 @@ export USER_NAME=${USER_NAME:-$USER}
 sudo chsh -s "$(which zsh)" "$USER_NAME"
 
 # Install nodejs version and make the default
-NODE_VERSION=14.14.0
+NODE_VERSION=16.12.0
 eval "$(fnm env)"
-fnm install $NODE_VERSION \
-  && fnm use $NODE_VERSION \
-  && fnm default $NODE_VERSION
+fnm install "$NODE_VERSION" \
+  && fnm use "$NODE_VERSION" \
+  && fnm default "$NODE_VERSION"
 
