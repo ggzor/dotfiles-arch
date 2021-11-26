cdb6bd4a888c83dd7a9ba1ba5885937c8075a5da
ggzor <30713864+ggzor@users.noreply.github.com>
2021-11-25 20:22:49 -0600

    Adding color picker to Arch

--- rofi/scripts/launch.sh
+++ rofi/scripts/launch.sh
@@ -20,18 +20,22 @@ PROGRAMS=(
   [" virtualbox"]="virtualbox"
   [" vscode"]="code"
   [" vscode insiders"]="code-insiders"
+  [" pick color"]="xcolor -P 80 -S 4 -s"
 )
 
 if [ "$@" ]
 then
-    "${PROGRAMS["$*"]}" &> /dev/null &
+    ${PROGRAMS["$*"]} &> /dev/null &
 else
     echo -en "\x00prompt\x1f<span fgcolor='#6c7a89'>launch</span>\n"
     echo -en "\x00markup-rows\x1ftrue\n"
 
     for PROGRAM in "${!PROGRAMS[@]}"; do
-        command -v "${PROGRAMS[$PROGRAM]}" &> /dev/null && \
-            echo "$PROGRAM"
+      if command -v "${PROGRAMS[$PROGRAM]}" &> /dev/null; then
+        echo "$PROGRAM"
+      elif [[ "$PROGRAM" == *color* ]]; then
+        echo "$PROGRAM"
+      fi
     done
 fi
 
