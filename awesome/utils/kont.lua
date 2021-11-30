083e1f0c15fe0794de7aa317bcd42502d0855cbb
ggzor <30713864+ggzor@users.noreply.github.com>
2021-11-30 09:17:18 -0600

    CPS for lua

---
+++ awesome/utils/kont.lua
@@ -0,0 +1,61 @@
+-- Utils for Continuation Passing Style (CPS) things
+-- type Cont a = (a -> r) -> r
+
+local mod = {}
+
+-- Map a continuation
+-- (a -> b) -> Cont a -> Cont b
+function mod.map(func, cont_src)
+    return function (cont)
+        cont_src(function (result)
+            cont(func(result))
+        end)
+    end
+end
+
+-- Return a value wrapped in a continuation
+-- a -> Cont a
+function mod.pure(value)
+    return function (cont)
+        cont(value)
+    end
+end
+
+-- Apply a function wrapped in a continuation
+-- to a continuation value
+-- Cont (a -> b) -> Cont a -> Cont b
+function mod.ap(cont_f, cont_a)
+    return function (cont)
+        cont_a(function (a)
+            cont_f(function (f)
+                cont(f(a))
+            end)
+        end)
+    end
+end
+
+-- Turn a list of continuations into a continuation of list.
+-- [Cont a] -> Cont [a]
+function mod.sequence(conts_list)
+    local key = next(conts_list)
+    if key == nil then
+        return mod.pure({})
+    else
+        local current = conts_list[key]
+        conts_list[key] = nil
+
+        local function f(result)
+            return function (l)
+                l[key] = result
+                return l
+            end
+        end
+
+        return mod.ap(
+                mod.map(f, current),
+                mod.sequence(conts_list))
+    end
+end
+
+return mod
+
