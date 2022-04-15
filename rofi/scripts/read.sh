a112ad79d3a49531583cb17b256a55597c74d8e4
ggzor <30713864+ggzor@users.noreply.github.com>
2022-04-15 08:40:10 -0500

    (rofi) Improve search speed

--- rofi/scripts/read.sh
+++ rofi/scripts/read.sh
@@ -4,14 +4,6 @@ shopt -s nullglob
 
 READ_DIR="$HOME/Documents/Books"
 
-LIBRARIES=( "$READ_DIR" )
-
-for dir in "$READ_DIR"/*; do
-    if [[ -d "$dir" ]]; then
-        LIBRARIES+=( "$dir" )
-    fi
-done
-
 if [ "$@" ]
 then
     zathura "$ROFI_INFO" &> /dev/null &
@@ -19,13 +11,14 @@ else
     echo -en "\x00prompt\x1f<span fgcolor='#6c7a89'>read</span>\n"
     echo -en "\x00markup-rows\x1ftrue\n"
 
-    for LIBRARY in "${LIBRARIES[@]}"; do
-        if [[ -d "$LIBRARY" ]]; then
-            LIB_NAME="$(basename "$LIBRARY")"
-            for FILE in "$LIBRARY"/*.{pdf,epub,djvu}; do
-                printf "<b>%s/</b>$(basename "$FILE")\x00info\x1f%s\n" "$LIB_NAME" "$FILE"
-            done
-        fi
+    find "$READ_DIR" -type f \
+             \(  -name '*.pdf' \
+             -or -name '*.epub' \
+             -or -name '*.djvu' \
+             \) -printf '%p\0%P\0' | \
+    while IFS= read -r -d '' FULL_PATH; do
+        IFS= read -r -d '' REL_PATH
+        printf "%s\x00info\x1f%s\n" "$REL_PATH" "$FULL_PATH"
     done
 fi
 
